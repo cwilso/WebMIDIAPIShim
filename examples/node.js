830e833b57b9b40c9c06352b520725bb12df7508
abudaan <daniel@abumarkub.net>
2017-09-27 23:16:41 +0200

    node version implemented

--- examples/node.js
+++ examples/node.js
@@ -1,58 +1,59 @@
 require('../node/index.js');
 
-let ins;
-let outs;
+let midi;
+let inputs;
+let outputs;
 
 function onMIDIFailure(msg) {
     console.log(`Failed to get MIDI access - ${msg}`);
+    process.exit(1);
 }
 
 function onMIDISuccess(midiAccess) {
     midi = midiAccess;
-    ins = midi.inputs;
-    outs = midi.outputs;
-    // setTimeout(testOutputs, 200);
-    setTimeout(testInputs, 200);
+    inputs = midi.inputs;
+    outputs = midi.outputs;
+    setTimeout(testOutputs, 500);
 }
 
 function testOutputs() {
     console.log('Testing MIDI-Out ports...');
-    for (const i in outs) {
-        const x = outs[i];
-        console.log(x);
-        console.log('id:', x.id, 'manufacturer:', x.manufacturer, 'name:', x.name, 'version:', x.version);
-        x.send([0x90, 60, 0x7f]);
-    }
+    outputs.forEach((port) => {
+        console.log('id:', port.id, 'manufacturer:', port.manufacturer, 'name:', port.name, 'version:', port.version);
+        port.open();
+        port.send([0x90, 60, 0x7f]);
+    });
     setTimeout(stopOutputs, 1000);
 }
 
 function stopOutputs() {
-    for (const i in outs) {
-        outs[i].send([0x80, 60, 0]);
-    }
+    outputs.forEach((port) => {
+        port.send([0x80, 60, 0]);
+    });
     testInputs();
 }
 
 function onMidiIn(ev) {
     const arr = [];
-    for (let i = 0; i < ev.data.length; i++) arr.push((ev.data[i] < 16 ? '0' : '') + ev.data[i].toString(16));
+    for (let i = 0; i < ev.data.length; i++) {
+        arr.push((ev.data[i] < 16 ? '0' : '') + ev.data[i].toString(16));
+    }
     console.log('MIDI:', arr.join(' '));
 }
 
 function testInputs() {
     console.log('Testing MIDI-In ports...');
-    for (const i in ins) {
-        const x = ins[i];
-        console.log('id:', x.id, 'manufacturer:', x.manufacturer, 'name:', x.name, 'version:', x.version);
-        x.onmidimessage = onMidiIn;
-    }
+    inputs.forEach((port) => {
+        console.log('id:', port.id, 'manufacturer:', port.manufacturer, 'name:', port.name, 'version:', port.version);
+        port.onmidimessage = onMidiIn;
+    });
     setTimeout(stopInputs, 5000);
 }
 
 function stopInputs() {
     console.log('Thank you!');
     navigator.close(); // This will close MIDI inputs, otherwise Node.js will wait for MIDI input forever.
+    process.exit(0);
 }
 
 navigator.requestMIDIAccess().then(onMIDISuccess, onMIDIFailure);
-
